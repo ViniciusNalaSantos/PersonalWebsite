730f6508210e5c089b4123c3f89d2793a795902c
Vinicius Nala Santos <vinicius.nala@gmail.com>
2024-11-27 20:50:09 -0300

    Starting contact screen development.

---
+++ frontend/src/components/Contact/index.js
@@ -0,0 +1,35 @@
+import "./contact.css"
+
+function Contact() {
+  return (
+    <div>
+        <div>
+            <div>
+                <h1>Sinta-se livre para entrar</h1>
+                <h1>entrar em contato.</h1>
+                <h2>Seria ótimo conversar!</h2>
+                <section>Envie uma mensagem nas minhas mídias:</section>
+                <div>
+                    <icon></icon>
+                    <div>
+                        <span>Linkedln:</span>
+                        <a href="https://www.linkedin.com/in/viniciusnala/">https://www.linkedin.com/in/viniciusnala/</a>                    
+                    </div>
+                </div>
+                <div>
+                    <icon></icon>
+                    <div>
+                        <span>Email:</span>
+                        <a href="vinicius.nala@gmail.com">vinicius.nala@gmail.com</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div>
+            <h3>Mande uma mensagem!</h3>
+        </div>
+    </div>
+  )
+}
+
+export default Contact
