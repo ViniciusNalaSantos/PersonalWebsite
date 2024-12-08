132645fa617390a1baab9e215712ce4884e3861c
Vinicius Nala Santos <vinicius.nala@gmail.com>
2024-12-07 22:20:47 -0300

    Desenvolvimento seção projetos.

--- frontend/src/components/Projects/index.js
+++ frontend/src/components/Projects/index.js
@@ -18,45 +18,41 @@ function Projects() {
             <h2 id='projects-subtitle'>{subtitle}</h2>
             <div id='projects-cards'>
                 <Card className='projects-cards-card'>
-                    <Card.Img variant="top" src={ImageParametricNonParametric} />
+                    <Card.Img variant="top" src={ImageParametricNonParametric} className='projects-cards-card-image' />
                     <Card.Body>
-                        <Card.Title>Card Title</Card.Title>
-                        <Card.Text>
-                        Some quick example text to build on the card title and make up the
-                        bulk of the card's content.
+                        <Card.Title className='projects-cards-card-title'>Modelos ML Paramétricos x Não-paramétricos</Card.Title>
+                        <Card.Text className='projects-cards-card-subtitle'>
+                            Entendendo dois conceitos tão essenciais no mundo do Machine Learning
                         </Card.Text>
                     </Card.Body>
                 </Card>
 
                 <Card className='projects-cards-card'>
-                    <Card.Img variant="top" src={ImageGradientDescent} />
+                    <Card.Img variant="top" src={ImageGradientDescent}  className='projects-cards-card-image' />
                     <Card.Body>
-                        <Card.Title>Card Title</Card.Title>
-                        <Card.Text>
-                        Some quick example text to build on the card title and make up the
-                        bulk of the card's content.
+                        <Card.Title className='projects-cards-card-title'>Gradient Descent: Concept & Manual Python Implementation</Card.Title>
+                        <Card.Text className='projects-cards-card-subtitle'>
+                            Understanding Gradient Descent
                         </Card.Text>
                     </Card.Body>
                 </Card>
 
                 <Card className='projects-cards-card'>
-                    <Card.Img variant="top" src={ImageClustering3} />
+                    <Card.Img variant="top" src={ImageClustering3} className='projects-cards-card-image' />
                     <Card.Body>
-                        <Card.Title>Card Title</Card.Title>
-                        <Card.Text>
-                        Some quick example text to build on the card title and make up the
-                        bulk of the card's content.
+                        <Card.Title className='projects-cards-card-title'>Clustering: Extracting Patterns from Data & Cluster Interpretation + Plotly [Pt.3]</Card.Title>
+                        <Card.Text className='projects-cards-card-subtitle'>
+                            Customers segmentation based on their credit card usage behavior
                         </Card.Text>
                     </Card.Body>
                 </Card>
 
                 <Card className='projects-cards-card'>
-                    <Card.Img variant="top" src={ImageClustering2} />
+                    <Card.Img variant="top" src={ImageClustering2} className='projects-cards-card-image' />
                     <Card.Body>
-                        <Card.Title>Card Title</Card.Title>
-                        <Card.Text>
-                        Some quick example text to build on the card title and make up the
-                        bulk of the card's content.
+                        <Card.Title className='projects-cards-card-title'>Clustering: Extracting Patterns From Data & Metrics + Cluster Validation [Pt.2]</Card.Title>
+                        <Card.Text className='projects-cards-card-subtitle'>
+                            Customers segmentation based on their credit card usage behavior
                         </Card.Text>
                     </Card.Body>
                 </Card>
