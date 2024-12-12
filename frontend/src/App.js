9f6ccfd49013d856ec4f2a47028ec3abe5eccdeb
Vinicius Nala Santos <vinicius.nala@gmail.com>
2024-12-12 17:47:13 -0300

    Adding routes of HashRouter object.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,18 +5,24 @@ import About from './components/About';
 import Footer from './components/Footer';
 import Contact from './components/Contact';
 import Projects from './components/Projects';
+import { HashRouter, Routes, Route, BrowserRouter } from "react-router";
 
 
 function App() {
   return (
-    <div className='body'>
-      <Banner />
-      <Home />
-      <About />
-      <Contact />
-      <Projects />
-      <Footer />
-    </div>    
+    <div className='body'>      
+      <HashRouter>
+        <Banner />  
+        <Routes>
+          <Route path='/' element={ <Home /> } />
+          <Route path='/about' element={ <About /> } />
+          <Route path='/contact' element={ <Contact /> } />
+          <Route path='/projects' element={ <Projects /> } />          
+          <Route path='*' element={<div>Página não encontrada</div>} />          
+        </Routes>
+        <Footer />
+      </HashRouter>      
+    </div>   
   );
 }
 
