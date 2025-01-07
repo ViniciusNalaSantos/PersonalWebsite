bce4cf8cb5afc1a5b63f3803db47a8e725525b75
Vinicius Nala Santos <vinicius.nala@gmail.com>
2025-01-06 22:39:13 -0300

    Styling the form field of contact page.

--- frontend/src/components/FormField/index.js
+++ frontend/src/components/FormField/index.js
@@ -4,7 +4,7 @@ import Form from 'react-bootstrap/Form';
 function FormField({ children, as, rows }) {
   return (
       <Form.Group className="mb-3">                    
-        <Form.Control placeholder={children} as={as} rows={rows} />                    
+        <Form.Control placeholder={children} as={as} rows={rows} className='form-control'/>                    
       </Form.Group>
   )
 }
